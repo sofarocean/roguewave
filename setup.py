b7d5c3bddfc0493cf1bab8c26e18727d9a0b58c8
pietersmit <pieter@sofarocean.com>
2022-05-30 20:42:02 -0700

    - bug fix in downloading data for multiple spotter

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
     name='roguewave',
-    version='0.1.9',
+    version='0.1.10',
     license='Apache 2 License',
     install_requires=[
         'pysofar',

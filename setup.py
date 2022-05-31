039dd440d4fc182bb707ac5608a4352b288e07f4
pietersmit <pieter@sofarocean.com>
2022-05-31 13:26:45 -0700

    Upped version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
     name='roguewave',
-    version='0.1.10',
+    version='0.1.11',
     license='Apache 2 License',
     install_requires=[
         'pysofar',

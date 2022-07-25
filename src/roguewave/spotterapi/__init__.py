3fc5d7c44e006dcb83ec8fff41e7ca189bce68b6
pietersmit <pieter@sofarocean.com>
2022-07-24 21:36:04 -0700

    easier import

--- src/roguewave/spotterapi/__init__.py
+++ src/roguewave/spotterapi/__init__.py
@@ -1,3 +1,4 @@
 from . import exceptions
-from .spotterapi import get_spectrum, get_bulk_wave_data, get_data, get_spotter_ids
+from .spotterapi import get_spectrum, get_bulk_wave_data, get_data, \
+    get_spotter_ids, search_rectangle, search_circle
 from . import spottersdcard

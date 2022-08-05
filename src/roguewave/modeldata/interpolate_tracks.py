5ad0b87584a491bb51a7d0e19f263202404c89e2
pietersmit <pieter@sofarocean.com>
2022-08-05 16:26:01 -0700

    bug fix if all data is NaN in interpolation.

--- src/roguewave/modeldata/interpolate_tracks.py
+++ src/roguewave/modeldata/interpolate_tracks.py
@@ -210,11 +210,54 @@ def interpolate_points_data_arrray(data_array: DataArray,
                                    ) \
         -> DataArray:
     """
-    Interpolate a data array from a dataset at given points.
-    :param data_array:
-    :param _points:
-    :param cyclic_coordinates:
-    :return:
+    Interpolate a data array from a dataset at given points in N-dimensions.
+
+    The interpolation function takes care of the following edge cases not
+    covered by standard xarray interpolation:
+
+    1) Interpolation along cyclic dimensions (e.g. Longitude interpolating
+       across prime- or anti-meridian)
+    2) Interpolation of cyclic data. E.g. directions.
+    3) Interpolation near non-existing values (e.g. NaN values representing
+       land).
+
+    When these are covered by standard xarray interpolation we should
+    definitely switch.
+
+    :param data_array: xarray data array.
+
+    :param _points: dictionary with data_array coordinate names as keys and
+                    a numpy array with N entries. With coordiantes lat/lon:
+                    {
+                        lat: [ lat0, lat1 ,lat2, .., 10]
+                        lon: [ [lon0 ,lon1,lon2, .., lonN]
+                        time: [t0,t1,t2, ... tN]
+                    }
+                    where the points we are interolating are formed by the
+                    tuples [  (t[0],lat[0],lon[0]),
+                                 ....
+                              (t[1],lat[N],lon[N]),
+                    ]
+
+    :param independent_variable: coordinate that is used to parametrize the
+        points, usually time. This variable is used as the output coordinate
+        for the returned data array. If None and 'time' is a valid coordinate
+        'time' is used, otherwise it defaults to whatever the first returned
+        coordinate is from data_array.dims
+
+    :param cyclic_coordinates: Dictonary that contains as keys the coordinate
+        names of cyclic coordinates, and as value the cyclic length of the
+        coordinate.
+
+    :param cyclic_length_data: Cyclic length of the data. If data is not cyclic
+        this is set to None (default).
+
+    :param fractional_cyclic_range: range of the cyclic data expressed as a
+        fraciton of the cyclic lenght. E.g for angles in [-pi,pi] with
+        cyclic length 2*pi we would give [-0.5,0.5]. Defaults to [0,1].
+
+    :return: A data array of interpolated values with as coordinates the
+        specified independent coordinate (usually time).
     """
 
     # Get dimensions of the data array
@@ -292,7 +335,7 @@ def interpolate_points_data_arrray(data_array: DataArray,
             value[mask] = 0
             weights_sum += weight
             total  += value*weight
-        output[:] += total / weights_sum
+        output[:] = numpy.where(weights_sum>0,total / weights_sum,numpy.nan)
     else:
         # If this is a cyclic variable we are interpolating we will do a vector
         # interpolation of the unit vectors.
@@ -311,8 +354,8 @@ def interpolate_points_data_arrray(data_array: DataArray,
             # add weighted contribution
             cosine += weight * numpy.cos(value_radians)
             sine += weight * numpy.sin(value_radians)
-        cosine = cosine/weights_sum
-        sine = sine/weights_sum
+        cosine = numpy.where(weights_sum>0,cosine/weights_sum, numpy.nan)
+        sine = numpy.where(weights_sum>0,sine/weights_sum,numpy.nan)
 
         # Result is not necessarily unit vector, can be zero. In the latter
         # case the arctan2 simply returns 0.0. Here we will return NaN instead

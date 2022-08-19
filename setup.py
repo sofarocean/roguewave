20daae3b7ca745c252c105a0c4c7599273cfe4db
pietersmit <pieter@sofarocean.com>
2022-08-19 11:50:42 -0700

    Bugfix if there is only 1 returned data point in time for a particular spotter.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
     name='roguewave',
-    version='0.1.47',
+    version='0.1.48',
     license='Apache 2 License',
     install_requires=[
         'pysofar>=0.1.13',

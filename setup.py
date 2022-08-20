b684f05452006a7cd4630c6c217f2922e066f74c
pietersmit <pieter@sofarocean.com>
2022-08-20 09:55:12 -0700

    version update for release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
     name='roguewave',
-    version='0.1.51',
+    version='0.1.52',
     license='Apache 2 License',
     install_requires=[
         'pysofar>=0.1.13',

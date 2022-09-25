7a8f4262290864a4d436727bd8ed308d646b941f
pietersmit <pieter@sofarocean.com>
2022-09-25 13:34:03 -0700

    Formatter/Black

--- src/roguewave/wavewatch3/__init__.py
+++ src/roguewave/wavewatch3/__init__.py
@@ -1,11 +1,13 @@
-from roguewave.wavewatch3.restart_file import RestartFile, RestartFileStack
-from roguewave.wavewatch3.io import clone_restart_file, open_restart_file, \
-    write_partial_restart_file, write_restart_file, \
-    reassemble_restart_file_from_parts, \
-    open_restart_file_stack
+from roguewave.wavewatch3.restart_file import RestartFile, RestartFileTimeStack
+from roguewave.wavewatch3.io import (
+    clone_restart_file,
+    open_restart_file,
+    write_partial_restart_file,
+    write_restart_file,
+    reassemble_restart_file_from_parts,
+    open_restart_file_stack,
+)
 
 from roguewave.wavewatch3.resources import create_resource
-from roguewave.wavewatch3.restart_file_metadata import read_header, \
-    MetaData
-from roguewave.wavewatch3.model_definition import read_model_definition, \
-    Grid
+from roguewave.wavewatch3.restart_file_metadata import read_header, MetaData
+from roguewave.wavewatch3.model_definition import read_model_definition, Grid
--- tests/restart_files/__init__.py
+++ tests/restart_files/__init__.py
@@ -1,16 +1,16 @@
 import hashlib
-from roguewave.wavewatch3 import clone_restart_file, RestartFile,\
-    open_restart_file
+from roguewave.wavewatch3 import clone_restart_file, RestartFile, open_restart_file
 from io import BytesIO
 import os
-
-REMOTE_HASH = '811449d693c24cca014fbe212a9db011'
-REMOTE_RESTART_FILE = 's3://sofar-wx-data-dev-os1/roguewave/restart001.ww3'
-REMOTE_MOD_DEF = 's3://sofar-wx-data-dev-os1/roguewave/mod_def.ww3'
-TEST_DIR = '.'
-LOCAL_FILE_NAME = 'restart001.ww3'
-REMOTE_WRITE_PATH = 's3://sofar-wx-data-dev-os1/roguewave/partial/'
 import boto3
+
+REMOTE_HASH = "811449d693c24cca014fbe212a9db011"
+REMOTE_RESTART_FILE = "s3://sofar-wx-data-dev-os1/roguewave/restart001.ww3"
+REMOTE_MOD_DEF = "s3://sofar-wx-data-dev-os1/roguewave/mod_def.ww3"
+TEST_DIR = "."
+LOCAL_FILE_NAME = "restart001.ww3"
+REMOTE_WRITE_PATH = "s3://sofar-wx-data-dev-os1/roguewave/partial/"
+
 
 def file_hash(file):
     with open(file, "rb") as f:
@@ -20,19 +20,22 @@ def file_hash(file):
 
     return file_hash.hexdigest()
 
+
 def clone_remote() -> RestartFile:
     #
     local_file = os.path.join(TEST_DIR, LOCAL_FILE_NAME)
-    if not os.path.exists( local_file ):
-        clone_restart_file(REMOTE_RESTART_FILE,
-                           REMOTE_MOD_DEF,
-                           os.path.join(TEST_DIR, LOCAL_FILE_NAME))
+    if not os.path.exists(local_file):
+        s3 = boto3.client("s3")
+        s3.download_file(
+            "sofar-wx-data-dev-os1", "roguewave/restart001.ww3", local_file
+        )
 
-    local_mod_file = os.path.join(TEST_DIR,'mod_def.ww3')
+    local_mod_file = os.path.join(TEST_DIR, "mod_def.ww3")
     if not os.path.exists(local_mod_file):
-        s3 = boto3.client('s3')
-        s3.download_file('sofar-wx-data-dev-os1',
-                         'roguewave/mod_def.ww3', local_mod_file)
+        s3 = boto3.client("s3")
+        s3.download_file(
+            "sofar-wx-data-dev-os1", "roguewave/mod_def.ww3", local_mod_file
+        )
 
     return open_restart_file(local_file, local_mod_file)
 

1a7150ea97a21c1add11e903181e99b5a2b3dadf
pietersmit <pieter@sofarocean.com>
2023-03-06 23:03:42 -0800

    Spline based peak period

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="roguewave",
-    version="0.2.6",
+    version="0.2.7",
     license="Apache 2 License",
     install_requires=[
         "pysofar>=0.1.13",

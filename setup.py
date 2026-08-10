72c80288c625ed14bc70cefa78c3b4f488ee4f9f
pieterbartsmit <pieterbartsmit@gmail.com>
2026-08-10 13:32:33 -0700

    Up Version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="roguewave",
-    version="0.2.32",
+    version="2026.08.10",
     license="Apache 2 License",
     install_requires=[
         "pysofar>=0.1.13",
